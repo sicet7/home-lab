bf26959e394bfbed70c531224e28d1b34f0050e9
Martin René Sørensen <git@sicet7.com>
2025-12-30 23:18:57 +0100

    WIP: started working on opencloud setup.

---
+++ lxc/nixos/opencloud/configuration.nix
@@ -0,0 +1,31 @@
+{ config, modulesPath, pkgs, lib, ... }: 
+{ 
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
+
+  nix.settings = { sandbox = false; };
+
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [  ];
+    allowedUDPPorts = [  ];
+  };
+
+  security.pam.services.sshd.allowNullPassword = lib.mkForce false;
+  services.fstrim.enable = false;
+  services.openssh = {
+    enable = false;
+    openFirewall = false;
+  };
+
+  services.resolved.extraConfig = ''
+    Cache=true
+    CacheFromLocalhost=true
+  '';
+
+  system.stateVersion = "25.11";
+}
